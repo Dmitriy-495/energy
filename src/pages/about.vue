eef850c6383001710642502c6d4542cfdbe6fd1f
Dmitriy-495 <tda495@yandex.ru>
2025-05-23 15:24:23 +0300

    First commit

---
+++ src/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <p><NuxtLink to="/">Home</NuxtLink></p>  
+    <h1>This About Page</h1>
+    <p>This page will be displayed at the /about route.</p>
+  </section>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p><NuxtLink to="/about">About</NuxtLink></p>  
+    <h1>Welcome to the Home Page</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
