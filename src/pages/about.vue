48a78a3fd98e67c4af36b01b2ce4fd850e775646
Dmitriy-495 <tda495@yandex.ru>
2025-05-28 11:36:12 +0300

    Clear ALL

--- src/pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section>
-    <p><NuxtLink to="/">Home</NuxtLink></p>  
-    <h1>This About Page</h1>
-    <p>This page will be displayed at the /about route.</p>
-  </section>
-</template>
--- src/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <p><NuxtLink to="/about">About</NuxtLink></p>  
-    <h1>Welcome to the Home Page</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
-  </div>
-</template>
